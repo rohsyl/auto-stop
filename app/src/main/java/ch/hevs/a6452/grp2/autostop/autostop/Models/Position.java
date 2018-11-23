3ab7b6e6af505fdd28fa0699b60b06c8afb75848
Sylvain Roh <syzin12@gmail.com>
2018-11-23 12:10:39 +0100

    Add models (Plate, Position, Report, Trip)

---
+++ app/src/main/java/ch/hevs/a6452/grp2/autostop/autostop/Models/Position.java
@@ -0,0 +1,8 @@
+package ch.hevs.a6452.grp2.autostop.autostop.Models;
+
+public interface Position {
+    Double getLatitude();
+    Double getLongitude();
+    String getName();
+    Long getTimestamp();
+}
