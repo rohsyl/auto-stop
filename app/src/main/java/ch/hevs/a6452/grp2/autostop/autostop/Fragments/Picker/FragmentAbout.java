72a0ce7755892befebf0d4be42681d6fc13c8b5c
vladomitrovic <vlado.mitrovic@outlook.com>
2018-11-23 13:09:35 +0100

    Fragment nav bar + clean

--- app/src/main/java/ch/hevs/a6452/grp2/autostop/autostop/Fragments/Picker/FragmentAbout.java
+++ app/src/main/java/ch/hevs/a6452/grp2/autostop/autostop/Fragments/Picker/FragmentAbout.java
@@ -1,52 +1,28 @@
 package ch.hevs.a6452.grp2.autostop.autostop.Fragments.Picker;
 
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+
 import ch.hevs.a6452.grp2.autostop.autostop.R;
 
 
-public class FragmentStart extends Fragment {
+public class FragmentAbout extends Fragment {
 
-    public static final String TAG = "FragmentStart";
+    public static final String TAG = "FragmentAbout";
 
-    Button buttonStartTrip;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        View view = inflater.inflate(R.layout.fragment_start, container, false);
-
-        buttonStartTrip = (Button) view.findViewById(R.id.buttonStartTrip);
-
-        buttonStartTrip.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "ButtonStartTrip clicked");
-
-                /* TODO Implement the activity select Plate
-
-                Intent intent = new Intent(this, LocationActivity.class);
-                startActivity(intent);
-                */
-            }
-        });
-
-
-
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
 
         return view;
     }
@@ -65,5 +41,4 @@ public class FragmentStart extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         return true;
     }
-
 }
--- app/src/main/java/ch/hevs/a6452/grp2/autostop/autostop/Fragments/Picker/FragmentTrip.java
+++ app/src/main/java/ch/hevs/a6452/grp2/autostop/autostop/Fragments/Picker/FragmentTrip.java
@@ -1,11 +1,9 @@
 package ch.hevs.a6452.grp2.autostop.autostop.Fragments.Picker;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,40 +11,20 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+
 import ch.hevs.a6452.grp2.autostop.autostop.R;
 
 
-public class FragmentStart extends Fragment {
+public class FragmentTrip extends Fragment {
 
-    public static final String TAG = "FragmentStart";
+    public static final String TAG = "FragmentTrip";
 
-    Button buttonStartTrip;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        View view = inflater.inflate(R.layout.fragment_start, container, false);
-
-        buttonStartTrip = (Button) view.findViewById(R.id.buttonStartTrip);
-
-        buttonStartTrip.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "ButtonStartTrip clicked");
-
-                /* TODO Implement the activity select Plate
-
-                Intent intent = new Intent(this, LocationActivity.class);
-                startActivity(intent);
-                */
-            }
-        });
-
-
-
+        View view = inflater.inflate(R.layout.fragment_trip, container, false);
 
         return view;
     }
