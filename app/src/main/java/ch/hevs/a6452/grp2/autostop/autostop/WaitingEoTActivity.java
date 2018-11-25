6d57f6cd9615a3ed3435418b1032cb24670c384e
R-Men <huser.gaetan@hotmail.com>
2018-11-25 11:07:40 +0100

    EoT Activity creation

---
+++ app/src/main/java/ch/hevs/a6452/grp2/autostop/autostop/WaitingEoTActivity.java
@@ -0,0 +1,13 @@
+package ch.hevs.a6452.grp2.autostop.autostop;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class WaitingEoTActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waitingEoT);
+    }
+}
